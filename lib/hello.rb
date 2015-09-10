5da5a7fdcd092b438bddd1b86b44ce39851e7308
Joao Pereira <joaopapereira@gmail.com>
2015-09-10 10:23:06 -0400

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Joao Pereira (me@places.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
